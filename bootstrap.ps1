9b1382898ee7cddb0a39fb3e79418dafec3a51cf
Jon Winn <me@jonwinn.com>
2023-12-16 14:05:29 -0800

    feat: have homebrew install use Brewfile
    - pass the Brewfile into the install_homebrew command
    - start to implement windows based winget install process

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,25 @@
-@powershell
+# elevate powershell, if required
+# https://stackoverflow.com/a/57035712/2441655
+
+$principal = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent();
+$adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator;
+
+if (!$principal.IsInRole($adminRole)) {
+    # run the script elevated--preserving the working dir
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
 
 # update the Microsoft Store Apps
-Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+Get-CimInstance -Namespace root/CIMV2/mdm/dmmap -ClassName MDM_EnterpriseModernAppManagement_AppManagement01 | Invoke-CimMethod -MethodName UpdateScanMethod
+
+# TBD: the above command schedules the updates and returns,
+#      but doesn't wait for updates to complete
+# Note: it may be ideal for the winget commands to run as the current user
+
+# ensure the App Installer is installed, via the Microsoft Store
+
+# update winget sources
+winget source update
+
+# install apps, via winget
